cba248ee58b4183234fe12787b3a79a6d4981ee8
Jared Scott <jmsmm92@gmail.com>
2015-03-13 22:41:46 -0500

    The biggest changes are the String[][] grid is now Object[][]. Small changes were made to adjust to this. Everything has been tested to a degree

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -1,17 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grid;
 
 /**
  *
- * @author jms12_000
+ * @author Jared M Scott
+ * Update:3/13/15
+ * Its been decided that String[][] is too limiting. I am changing it to Object[][]
+ * so that a Character, Item, etc can be inserted into an [x, y] location
  */
 public class Grid {
-    
-    /**
-     * We need one sets of GrideElements (enums) for borders and contents 
-     */
+
+    //create a 2 vector grid, and two ints repreenting an axiz
+    int xAxis;
+    int yAxis;
+    Object[][] grid;
+
+    public Grid(int xAxis, int yAxis) {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+        grid = new Object[xAxis][yAxis];  
+    }
+  
+    //this method is no longer needed. THis kind of functionality should be 
+    //used in a controller for MVC
+    /*
+    void displayEntireGrid() {
+
+        for (int i = 0; i < this.xAxis; ++i) {
+            for (int j = 0; j < this.yAxis; ++j) {
+                System.out.print(getContent(i, j) + " ");
+            }
+            System.out.println();
+        }
+    }
+*/
+    Object getContent(int xx, int yy) {
+        if (xx > xAxis || xx < 0 || yy > yAxis || yy < 0) {
+            System.err.println("Error: the x and/or y location is out of bounds");
+            return null;
+        }   
+        return this.grid[xx][yy];
+    }
+
+    //TODO add error checking for setters and getters
+    public void setxAxis(int xAxis) {
+        this.xAxis = xAxis;
+    }
+
+    public void setyAxis(int yAxis) {
+        this.yAxis = yAxis;
+    }
+
+    public void setGridElement(int x, int y, Object obj) {
+        this.grid[x][y] = obj;
+    }
+
+    public int getxAxis() {
+        return xAxis;
+    }
+
+    public int getyAxis() {
+        return yAxis;
+    }
+
+    public Object[][] getGrid() {
+        return grid;
+    }
 }
--- src/Grid/GridController.java
+++ src/Grid/GridController.java
@@ -1,14 +1,122 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grid;
 
 /**
  *
- * @author jms12_000
+ * @author Jared M Scott
+ * Update: 3/13/15
+ * A Border class has been added to server as the edges of the map
  */
 public class GridController {
+
+    Grid grid;
+    Border border;
     
+    GridController(int x, int y) {
+        border = new Border();
+        if(initializeGrid(x, y) != 1)
+            System.err.println("Error: grid was not initialized");
+    }
+
+    public class Border {
+        String name = "Border";
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+    
+    /*
+    Inititialized the Grid (duh), and calls createBorder
+    */
+    int initializeGrid(int x, int y) {
+        if (x < 5 || y < 5) {
+            System.err.println("Error: must be nto smaller than 5 x 5");
+            return 0;
+        }
+
+        grid = new Grid(x, y);
+
+        //create the border from here
+        if (createBorder(x, y) != 1) {
+            System.err.println("Error: Could not create border");
+        }
+
+        return 1;
+    }
+
+    //create a method that makes the border by adding an instance of
+    //Border border in the proper gird locations
+    //returns 1 when completed. 
+    int createBorder(int x, int y) {
+        //System.out.println("Setting border");
+
+        int row, col;
+        
+        //[row0, col0] -> [row0, colmax - 1];   
+        for (col = 0; col < getGrid().getyAxis() - 1; ++col) {
+            getGrid().setGridElement(0, col, border);
+        }
+        
+        //System.out.println("Part 1 done ");
+
+        //[row0, col0] ->[rowmax - 1, col0]
+        for (row = 0; row < getGrid().getxAxis() - 1; ++row) {
+            getGrid().setGridElement(row, 0, border);
+        }
+        //System.out.println("Part 2 done");
+
+        
+        //[rowMax - 1, col0] -> [rowMax, colMax - 1];
+        for (col = 0; col < getGrid().getyAxis() - 1; ++col) {
+            getGrid().setGridElement(getGrid().getxAxis() - 1, col, border);
+        }
+                
+        //System.out.println("Part 3 done");
+
+        
+        //[row0, colMax - 2] ->[rowMax -1, colMax - 1]
+        for (row = 0; row < getGrid().getxAxis(); ++row) {
+            getGrid().setGridElement(row, getGrid().getyAxis() - 1, border);
+        }
+                
+        //System.out.println("Part 4 done");
+        return 1;
+    }
+
+    /*
+    This was created soley for testing purposes.
+    Displays each element in grid via for loops and toString
+    null cant use to string so i create an if statement as a workaround
+    */
+    public void display() {
+        for(int i = 0; i < getGrid().getxAxis(); ++i){
+            for(int j = 0; j < getGrid().getyAxis(); ++j){
+                String s;
+                if(getGrid().getContent(i, j) != null){
+                s = getGrid().getContent(i, j).toString();
+               // System.out.print("Does not == null");
+                }
+                else 
+                    s = "null";
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    //remove enemy/player/item
+    public void emptyGridLocation(int x, int y) {
+        grid.setGridElement(x, y, null);
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    //we'll only need this if we're changing levels i think
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
 }
