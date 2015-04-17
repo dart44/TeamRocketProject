404020ec46049773f7dffc6147f0eb4b190b01b1
UpasakhaJason <upasakhajason@gmail.com>
2015-04-17 14:26:40 -0500

    GridController test class.  This is a prototype, and more sophisticated tests are to follow.

--- src/TeamRocketProject/TeamRocketProject.java
+++ src/TeamRocketProject/TeamRocketProject.java
@@ -8,6 +8,10 @@ import java.applet.Applet;
 import grid.Grid;
 import grid.GridController;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Iterator;
+import Character.Character;
+
 /**
  *
  * @author Jeremy Crook
@@ -16,8 +20,33 @@ public class TeamRocketProject extends Applet implements Runnable {
     /* TeamRocketProject class constructor */
     public TeamRocketProject(){
         //TODO Create ArrayList of Character objects
+        /* Get number of characters.  Use even number so both 
+           players have the same number of characters
+        */
+        Random r = new Random(System.currentTimeMillis());
+        int x;
+        int[] pos;
+        ArrayList<Character> allChars = new ArrayList<Character>();
+        Character P1Char1 = new Character("Player1");
+        Character P1Char2 = new Character("Player1");
+        Character P2Char1 = new Character("Player2");
+        Character P2Char2 = new Character("Player2");
+        allChars.add(P1Char1);
+        allChars.add(P1Char2);
+        allChars.add(P2Char1);
+        allChars.add(P2Char2);
         GridController gridController = new GridController(10, 10);
         //TODO Randomly distribute characters onto grid
+        /* Wish list: function to return total number of cells in grid */
+        Iterator i = allChars.iterator();
+        // initialize x
+        x = r.nextInt(100);
+        while(i.hasNext()){
+          while(!gridController.CheckValidSpace(pos[x])){
+            x = r.nextInt(100);
+          }
+        }
+        
         //TODO Initialize TurnOrder by passing it Character ArrayList
         //TODO Pass control to Player who owns TurnCharacter
     }
